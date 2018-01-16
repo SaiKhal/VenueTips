bcc5e733f6bb7f091c1ff9359afc44d0f4e2fb60
Iram Fattah <iramfattah94@gmail.com>
2018-01-16 17:17:26 -0500

    network helper

---
+++ VenueTips/AppError.swift
@@ -0,0 +1,18 @@
+//
+//  AppError.swift
+//  VenueTips
+//
+//  Created by C4Q on 1/16/18.
+//  Copyright © 2018 Masai Young. All rights reserved.
+//
+
+import Foundation
+enum AppError: Error {
+    case badURL(url: String)
+    case badImageURL(url: String)
+    case badData
+    case badResponseCode(code: Int)
+    case cannotParseJSON(rawError: Error)
+    case noInternet
+    case other(rawError: Error)
+}
