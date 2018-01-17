e48e15b8f6a322ca7dacc4e96b9c348fc89c3367
Masai Young <masai.young@gmail.com>
2018-01-16 22:47:23 -0500

    Forgot to add a file

---
+++ VenueTips/New Group2/SearchBars/MainCustomSearchBar.swift
@@ -0,0 +1,40 @@
+//
+//  MainCustomSearchBar.swift
+//  VenueTips
+//
+//  Created by Masai Young on 1/16/18.
+//  Copyright © 2018 Masai Young. All rights reserved.
+//
+
+import UIKit
+
+class MainCustomSearchBar: UISearchBar {
+    
+    lazy var searchBar: UISearchBar = {
+        let sb = UISearchBar()
+        sb.searchBarStyle = .prominent
+        sb.isTranslucent = false
+        sb.backgroundColor = .white
+        sb.tag = 0
+        sb.placeholder = "Search for a venue"
+        return sb
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    func commonInit() {
+        setupViews()
+    }
+    
+    func setupViews() {
+        self.addSubview(searchBar)
+    }
+}
